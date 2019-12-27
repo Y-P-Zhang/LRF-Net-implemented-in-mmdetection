28e6d7d0ce2f01b16265da065aa0a8ca17a6ef95
Yipeng Zhang <38198932+Y-P-Zhang@users.noreply.github.com>
2019-12-27 11:04:05 +0800

    Update __init__.py

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,7 +2,6 @@ from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
-from .sen_test import SEN
 from .LRF_300 import LRFNet300
 from .LRF_512 import LRFNet512
 
